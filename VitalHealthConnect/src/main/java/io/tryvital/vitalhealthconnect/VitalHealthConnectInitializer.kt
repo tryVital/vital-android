9c369864d722a229096b6f63457037e1afe7ae07
Anders Ha <anders@andersha.com>
2024-03-18 18:58:32 +0000

    VIT-6014: Improve App Startup integration & shift required manifest declarations to the library manifest (#97)

---
+++ VitalHealthConnect/src/main/java/io/tryvital/vitalhealthconnect/VitalHealthConnectInitializer.kt
@@ -0,0 +1,24 @@
+package io.tryvital.vitalhealthconnect
+
+import android.content.Context
+import androidx.lifecycle.ProcessLifecycleInitializer
+import androidx.startup.Initializer
+import androidx.work.WorkManagerInitializer
+import io.tryvital.client.BuildConfig
+import io.tryvital.client.utils.VitalLogger
+
+/**
+ * AndroidX Startup Initializer for [VitalHealthConnectManager.getOrCreate].
+ */
+@Suppress("unused")
+class VitalHealthConnectInitializer: Initializer<VitalHealthConnectManager> {
+    override fun create(context: Context): VitalHealthConnectManager {
+        VitalLogger.getOrCreate().enabled = BuildConfig.DEBUG
+        return VitalHealthConnectManager.getOrCreate(context)
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> = mutableListOf(
+        WorkManagerInitializer::class.java,
+        ProcessLifecycleInitializer::class.java,
+    )
+}
