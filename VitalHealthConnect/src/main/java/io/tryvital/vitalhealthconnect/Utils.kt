f0c506826144ffef5e9b53ec84f32cb0af168128
Anders Ha <anders@tryvital.io>
2024-03-22 15:35:24 +0000

    VIT-6048: Fix conflicting monitoringTypes prefs key

--- VitalHealthConnect/src/main/java/io/tryvital/vitalhealthconnect/Utils.kt
+++ VitalHealthConnect/src/main/java/io/tryvital/vitalhealthconnect/Utils.kt
@@ -3,6 +3,7 @@ package io.tryvital.vitalhealthconnect
 import android.content.Context
 import io.tryvital.vitalhealthconnect.model.VitalResource
 import io.tryvital.vitalhealthconnect.model.WritableVitalResource
+import io.tryvital.vitalhealthconnect.workers.syncStateKey
 
 object UnSecurePrefKeys {
     internal const val loggerEnabledKey = "loggerEnabled"
@@ -13,9 +14,12 @@ object UnSecurePrefKeys {
     internal const val nextAlarmAtKey = "nextAlarmAt"
     internal const val lastAutoSyncedAtKey = "lastAutoSyncedAt"
     internal const val lastSeenWorkIdKey = "lastSeenWorkId"
-    internal const val typesMonitoredByChangesTokenKey = "typesMonitoredByChangesToken"
     internal const val autoSyncThrottleKey = "autoSyncThrottle"
     internal const val backgroundSyncMinIntervalKey = "backgroundSyncMinInterval"
+
+    internal fun syncStateKey(resource: VitalResource) = "sync-state.${resource.name}"
+    internal fun monitoringTypesKey(resource: VitalResource) = "monitoringTypes.${resource.name}"
+
     internal fun readResourceGrant(resource: VitalResource) = "resource.read.$resource"
     internal fun writeResourceGrant(resource: WritableVitalResource) = "resource.write.$resource"
 }
