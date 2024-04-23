f860f2b0b0860cebef4a0fc878ea2dfbcc09e405
Anders Ha <anders@tryvital.io>
2024-04-23 11:37:17 +0100

    SDK release 2.0.2

--- VitalClient/src/main/java/io/tryvital/client/VitalClient.kt
+++ VitalClient/src/main/java/io/tryvital/client/VitalClient.kt
@@ -175,7 +175,7 @@ class VitalClient internal constructor(context: Context) {
     }
 
     companion object {
-        const val sdkVersion = "2.0.1"
+        const val sdkVersion = "2.0.2"
 
         private var sharedInstance: VitalClient? = null
 
