c593ec874a66359c18a5c02e5f2be759a45be75d
Anders Ha <anders@tryvital.io>
2024-06-14 20:25:18 +0100

    3.0.0-beta.2

--- VitalClient/src/main/java/io/tryvital/client/VitalClient.kt
+++ VitalClient/src/main/java/io/tryvital/client/VitalClient.kt
@@ -127,7 +127,7 @@ class VitalClient internal constructor(context: Context) {
     }
 
     companion object {
-        const val sdkVersion = "3.0.0-beta.1"
+        const val sdkVersion = "3.0.0-beta.2"
 
         private var sharedInstance: VitalClient? = null
 
