6be5883367bfd7e4fd1c09097da43fce465bf5ea
Anders Ha <anders@junction.com>
2025-06-17 22:54:14 +0100

    Release 4.2.0-beta.1

--- VitalClient/src/main/java/io/tryvital/client/VitalClient.kt
+++ VitalClient/src/main/java/io/tryvital/client/VitalClient.kt
@@ -126,7 +126,7 @@ class VitalClient internal constructor(context: Context) {
     }
 
     companion object {
-        const val sdkVersion = "4.1.4"
+        const val sdkVersion = "4.2.0-beta.1"
 
         private var sharedInstance: VitalClient? = null
         private val identifyMutex = Mutex()
