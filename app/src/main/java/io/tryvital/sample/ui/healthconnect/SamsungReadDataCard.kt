125229701a8f1f0ad477ff8986c01aefd14b550b
Anders Ha <anders@junction.com>
2026-09-18 11:51:38 +0100

    Add Samsung Health nutrition support

--- app/src/main/java/io/tryvital/sample/ui/healthconnect/SamsungReadDataCard.kt
+++ app/src/main/java/io/tryvital/sample/ui/healthconnect/SamsungReadDataCard.kt
@@ -79,7 +79,6 @@ private fun isSupportedBySamsungDataApi(resource: VitalResource): Boolean = when
     VitalResource.HeartRateVariability -> false
     VitalResource.MenstrualCycle -> false
     VitalResource.RespiratoryRate -> false
-    VitalResource.Meal -> false
     else -> true
 }
 
