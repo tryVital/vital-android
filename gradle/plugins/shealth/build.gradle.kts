b542893b9b07434eefd9d3eb40ea9ac686180cb9
Anders Ha <anders@tryvital.io>
2026-09-21 15:44:28 +0100

    Release 5.0.5 (#243)

--- gradle/plugins/shealth/build.gradle.kts
+++ gradle/plugins/shealth/build.gradle.kts
@@ -9,7 +9,7 @@ import org.gradle.api.tasks.compile.JavaCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 group = "io.tryvital"
-version = "5.0.4"
+version = "5.0.5"
 
 repositories {
     gradlePluginPortal()
@@ -75,7 +75,7 @@ publishing {
             artifactId = "shealth-plugins"
         }
 
-        version = "5.0.4"
+        version = "5.0.5"
 
         pom {
             name.set(
